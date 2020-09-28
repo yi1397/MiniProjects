e138eb8f04b65bfaff7777f14b778291700b6c0a
Jung Yuil <yi1397a@gmail.com>
2020-09-29 00:12:34 +0900

    200929

---
+++ face randmark and serial communication/SerialComm.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "serialport.h"
+
+#define RETURN_SUCCESS 1
+#define RETURN_FAIL 0
+
+
+class SerialComm
+{
+public:
+	SerialComm();
+	~SerialComm();
+
+	SerialPort	serial;
+	int		connect(const char* _portNum);
+	int		sendCommand(char pos);
+	int		sendCommand(const char* pos);
+	void		disconnect();
+};
